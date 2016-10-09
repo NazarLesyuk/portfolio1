a04f313561da36e37144028e992f13804c3fd5ec
Nazar Lesyuk <pronaza@gmail.com>
2016-10-09 19:26:51 +0300

    first commit

---
+++ front-end/sofimarsel/mail-3.php
@@ -0,0 +1,72 @@
+<?
+ // заносим в массив значение полей, их может быть больше
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+
+  mail("smpotolok@ya.ru", "Отзыв с сайта ".$_SERVER['HTTP_REFERER'],
+"\nИмя отправителя: ".$name."\nНомер телефона: ".$phone."\nСообщение: ".$msg); // письмо на свой электронный ящик, измените на свой email buahaga@ya.ru pronaza@yandex.ua
+
+Header("Refresh: 2; URL=".$_SERVER['HTTP_REFERER']); // спустя 8 секунд человек будет возвращён на предыдущий URL
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+<title>Данные успешно отправлены!</title>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+<meta name="robots" content="noindex"/>
+<style>
+body{
+  background: #ececec;
+}
+#wrapper {
+  min-width: 600px;
+  min-height: 500px;
+  height: 100%;
+    margin: 0 auto;
+    background: #ececec;
+  /*outline: 2px solid yellow;*/
+}
+#container {
+  margin: 0 auto;
+  margin-top: 100px;
+  width: 100%;
+  height: 100%;
+  /*outline: 1px solid red;*/
+}
+.notification {
+    width: 555px;
+    height: 350px;
+  margin: 0 auto;
+  /*outline: 1px solid blue;*/
+  background: url("images/Спасибо за доверие.png") no-repeat;
+  padding: 25px 0 0 0;
+}
+.notification label {
+  margin-left: 480px;
+}
+.notification h1 {
+  font: 23px/23px "Tahoma",sans-serif;
+  text-align: center;
+}
+.notification img {
+    width: 33%;
+    height: 50%;
+    border-radius: 50%;
+    margin-left: 180px;
+
+}
+
+</style>
+<div id="wrapper">
+  <div id="container">
+    <div class="notification">
+    </div>
+  </div>
+<script> // нажав на label посетитель вернётся на предыдущую страницу, где заполнял форму
+// document.getElementsByTagName('label')[0].onclick = function() {
+//   window.location.href="<? print $_SERVER['HTTP_REFERER']; ?>"
+// }
+</script>
